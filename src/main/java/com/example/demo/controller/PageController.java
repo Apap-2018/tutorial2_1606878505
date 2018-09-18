395a823175411e486fc40e358e66448635aedc73
Zaki Raihan <zaki.raihan98@gmail.com>
2018-09-18 14:32:23 +0700

    selesai latihan request parameter

--- src/main/java/com/example/demo/controller/PageController.java
+++ src/main/java/com/example/demo/controller/PageController.java
@@ -1,7 +1,9 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class PageController {
@@ -9,5 +11,11 @@ public class PageController {
 	@RequestMapping("/viral")
 	public String index() {
 		return "viral";
+	}
+	
+	@RequestMapping("/challange")
+	public String challange(@RequestParam(value= "name") String name, Model model) {
+		model.addAttribute("name", name);
+		return "challange";
 	}
 }
